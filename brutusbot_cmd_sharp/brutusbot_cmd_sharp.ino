712797621eef11379e78b77bb23f97c63b21f759
Gaurav Mathur <g@astro.fit>
2018-03-29 11:49:21 -0700

    Refactor code a bit

--- brutusbot_cmd_sharp/brutusbot_cmd_sharp.ino
+++ brutusbot_cmd_sharp/brutusbot_cmd_sharp.ino
@@ -32,36 +32,42 @@
 #define delay_per_degree 4
 #define reaction_delay 150
 #define pan_offset -8
-#define headlight1 19 //Headlight LEDs connected to Analog 5
-#define headlight2 18 //Headlight LEDs connected to Analog 5
+#define headlight1 19    // Headlight LEDs connected to Analog 5
+#define headlight2 18    // Headlight LEDs connected to Analog 5
 #define drivespeed 255
 #define reversespeed 225
 #define spinspeed 190
 
-#define NODRIVE 0  //Set to 1 to disable drive
+#define NODRIVE 0        // Set to 1 to disable drive
 
 // Setup objects from Libraries
 Servo servo_pan;
 
 // Initialize Global Variables
-int ledPin = 13;  // LED connected to digital pin 13
+int ledPin = 13;         // LED connected to digital pin 13
 int lastlook=0;
 int distance;
 int watchdog=0;
 unsigned long last_event = 0;
-int left_motor=150;  //Set left motor speed
-int right_motor=140;  // Set right motor speed
-int spin_speed=140;  // Motor Speed during a speed.
+int left_motor=150;      // Set left motor speed
+int right_motor=140;     // Set right motor speed
+int spin_speed=140;      // Motor Speed during a speed
+int rightTurnPin = 15;   // Indicate we need to right turn
+int leftTurnPin = 16;    // Indicate we need to turn left
+int turnRight = 0;
+int turnLeft = 0;
 
 // Function Declarations *****  (This helps read main code)
 int look(int dir);
 void drive(int command);
 void roamer(void);
-
+void checkDirection(void);
+void somethingAhead(void);
+void somethingAheadDuringRun(int straight, int left, int right);
 
 //Main Setup Function
-void setup() 
-{
+void setup() {
+  
   last_event=millis();
   //Randomize seed based on unconnected pin.
   randomSeed(analogRead(2));
@@ -79,24 +85,23 @@ void setup()
   digitalWrite(ledPin,LOW);
   digitalWrite(headlight1,LOW);
   digitalWrite(headlight2,LOW);
-  
+  pinMode(rightTurnPin, INPUT);
+  pinMode(leftTurnPin, INPUT);
 }
 
 
 
 //  ********************  Main Loop
-void loop()
-{
+void loop() {
   
-   roamer(); 
-  
+   roamer();
+
 }
 
 // Functions
 
 
-void drive(int command)
-{
+void drive(int command) {
   /*
   0 = all stop
   1 = drive forward
@@ -114,13 +119,12 @@ void drive(int command)
   rightspeed=drivespeed-20;
   leftspeed=drivespeed;
   
-  if (NODRIVE == 1)
-  {
+  if (NODRIVE == 1) {
     command=9;
   }
   
-  switch (command)
-  {
+  switch (command) {
+    
     case 0:
     // All Stop
     Serial.println("Stop");
@@ -189,15 +193,13 @@ void drive(int command)
     
   }
   
-  if (command != 0)
-  {
+  if (command != 0) {
     watchdog=0;
   }
   
 }
   
-int look(int dir)
-{
+int look(int dir) {
   /*This routine sets robot to look in a certain direction, then 
   measure distance at that direction.
   90 degrees is straight ahead
@@ -220,8 +222,7 @@ int look(int dir)
   
   delayMicroseconds(500);
   
-  while ( x < 8)
-  {
+  while ( x < 8) {
     sample=sample+analogRead(1);
     delayMicroseconds(200);   
     x++; 
@@ -231,38 +232,29 @@ int look(int dir)
   reading=sample/8;
   //Serial.printlnln(reading);
   
-  if (dir == 90)  //If We're looking ahead
-  {
-    if (reading > threshold)
-    {
+  if (dir == 90) { //If We're looking ahead
+    if (reading > threshold) {
       digitalWrite(ledPin, HIGH); //Head on object detected!
     }
-    else
-    {
+    else {
       digitalWrite(ledPin, LOW);  //No objects to worry about
     }
   }
   
-  if (dir == 135)  //If We're looking right
-  {
-    if (reading > (threshold*2))
-    {
+  if (dir == 135) { //If We're looking right
+    if (reading > (threshold*2)) {
       digitalWrite(ledPin, HIGH); //Object detected!
     }
-    else
-    {
+    else {
       digitalWrite(ledPin, LOW);  //No objects to worry about
     }
   }
   
-  if (dir == 45)  //If We're looking left
-  {
-    if (reading > (threshold*2))
-    {
+  if (dir == 45) { //If We're looking left
+    if (reading > (threshold*2)) {
       digitalWrite(ledPin, HIGH); //Head on object detected!
     }
-    else
-    {
+    else {
       digitalWrite(ledPin, LOW);  //No objects to worry about
     }
   }
@@ -272,9 +264,71 @@ int look(int dir)
   return(reading); 
 }
 
+void somethingAhead() {
+  drive(0);
+  int dir;
+  int right=look(135);
+  int left=look(45);
+  int straight=look(90);
+  if (right > left) {
+    //Turn left until clear
+    dir=3;
+  }
+  else {
+    //Turn Right until clear
+    dir=4;
+  }
+  drive(dir);
+  
+  while (straight > threshold) {
+    straight=look(90);
+  }
+  delay(reaction_delay);
+  drive(0);
+}
 
-void roamer() 
-{
+void somethingAheadDuringRun(int straight, int left, int right) {
+  drive(0);
+  if ((straight > threshold) || ((right > (threshold*2)) && (left > (threshold*2)))) {
+      //Interesting scenario - looked up and something is in our way!
+      //Backup, and reasses?
+      //Same if we are wedged left and right
+      drive(2);
+      while (straight > threshold) {
+        straight=look(90);
+      }
+      delay(reaction_delay);
+      drive(0);
+    }
+    else {
+      //Straight was fine, something is to left or right.
+      if (right > (threshold*2)) {
+        //Something off our Starboard Side Captain!
+        look(135);
+        drive(3);
+        while (right > (threshold*2)) {
+          right=look(135);
+        }
+        delay(reaction_delay);
+        drive(0);
+        look(90);
+      }
+      else {
+        //Something off our Port Side Captain!
+        look(45);
+        drive(4);
+        while (left > (threshold*2)) {
+          left=look(45);
+        }
+        delay(reaction_delay);
+        drive(0);
+        look(90);
+      }
+      
+    }
+}
+
+void roamer() {
   int left;
   int straight;
   int right;
@@ -284,107 +338,39 @@ void roamer()
   unsigned long last_looked_around;
   
   last_looked_around=millis()-5000;
-  while (1)
-  {
+  while (1) {
     straight=look(90);
-    if (straight > threshold)
-      {
+    if (straight > threshold) {
         // Something Ahead!
-        drive(0);
-        right=look(135);
-        left=look(45);
-        straight=look(90);
-        if (right > left)
-        {
-          //Turn left until clear
-          dir=3;
-        }
-        else
-        {
-          //Turn Right until clear
-          dir=4;
-        }
-        drive(dir);
-        
-        while (straight > threshold)
-        {
-          straight=look(90);
-        }
-        delay(reaction_delay);
-        drive(0);
-      }
-    else
-      {
+        somethingAhead();
+    } else {
         // Nothing Ahead!
-        if ((millis() - last_looked_around) < 2000)
-        {
-          //Servo should be tilted down - check left and right all the time
+        if ((millis() - last_looked_around) < 2000) {
+          // Servo should be tilted down - check left and right all the time
           drive(1);
-        }
-        else
-        {
+        } else {
           right=look(135);
           left=look(45);
           straight=look(90);
     
-          if ((right < (threshold*2)) && (left < (threshold*2)) && (straight < threshold))
-          {
-            //Nothing In front!  Free and Clear!
+          if ((right < (threshold*2)) && (left < (threshold*2)) && (straight < threshold)) {
+            // Nothing In front!  Free and Clear!
             last_looked_around=millis();
-          }
-          else
-          {
-            drive(0);
-            if ((straight > threshold) || ((right > (threshold*2)) && (left > (threshold*2))))
-              {
-                //Interesting scenario - looked up and something is in our way!
-                //Backup, and reasses?
-                //Same if we are wedged left and right
-                drive(2);
-                while (straight > threshold)
-                {
-                  straight=look(90);
-                }
-                delay(reaction_delay);
-                drive(0);
-              }
-              else
-              {
-                //Straight was fine, something is to left or right.
-                if (right > (threshold*2))
-                {
-                  //Something off our Starboard Side Captain!
-                  look(135);
-                  drive(3);
-                  while (right > (threshold*2))
-                  {
-                    right=look(135);
-                  }
-                  delay(reaction_delay);
-                  drive(0);
-                  look(90);
-                }
-                else
-                {
-                  //Something off our Port Side Captain!
-                  look(45);
-                  drive(4);
-                  while (left > (threshold*2))
-                  {
-                    left=look(45);
-                  }
-                  delay(reaction_delay);
-                  drive(0);
-                  look(90);
-                }
-                
-                
-              }
-                
-        }//End Some Threshold Passed Else
-         
-     } //End Haven't Looked Up Else
+          } else {
+            somethingAheadDuringRun(straight, left, right);
+          } //End Some Threshold Passed Else
+        } //End Haven't Looked Up Else
     }//End Nothing Ahead Else
   }//End While Loop
- }//End of Roamer Function
+}//End of Roamer Function
+
+void checkDirection() {
+  turnLeft = digitalRead(leftTurnPin);
+  turnRight = digitalRead(rightTurnPin);
+
+  Serial.print("Turn indicators - Left turn: ");
+  Serial.print(turnLeft);
+  Serial.print(" Right turn: ");
+  Serial.println(turnRight);
+}
 
